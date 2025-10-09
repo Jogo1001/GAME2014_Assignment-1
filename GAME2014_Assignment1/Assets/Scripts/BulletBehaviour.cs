a47be7225e706e107784c6724fdc88ca1740e571
Jogo1001 <101247726@georgebrown.ca>
2025-10-09 15:04:22 -0400

    adding bulletprefab, bullet behaviour, bullet manager

---
+++ GAME2014_Assignment1/Assets/Scripts/BulletBehaviour.cs
@@ -0,0 +1,30 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    Boundary verticalboundry;
+
+    Vector3 direction;
+
+    BulletManager manager;
+
+    void Start()
+    {
+        direction = transform.up;
+        manager = FindObjectOfType<BulletManager>();
+    }
+
+
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+        if (transform.position.y > verticalboundry.max)
+        {
+            manager.ReturnBullet(gameObject);
+        }
+    }
+}
---
+++ GAME2014_Assignment1/Assets/Scripts/BulletManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour
+{
+    [SerializeField]
+    int bulletTotal = 50;
+
+    GameObject bulletPrefab;
+    Queue<GameObject> bulletPool = new Queue<GameObject>();
+    void Start()
+    {
+        bulletPrefab = Resources.Load<GameObject>("Prefabs/Bullet");
+
+        for (int i = 0; i < bulletTotal; i++)
+        {
+            CreateBullet();
+        }
+    }
+    void CreateBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab);
+        bullet.SetActive(false);
+        bullet.transform.parent = transform;
+        bulletPool.Enqueue(bullet);
+    }
+    public GameObject GetBullet()
+    {
+        if (bulletPool.Count == 0)
+        {
+            Debug.Log("No bullet left in the queue");
+            CreateBullet();
+
+
+        }
+        GameObject bullet = bulletPool.Dequeue();
+        bullet.SetActive(true);
+        return bullet;
+
+    }
+
+    public void ReturnBullet(GameObject bullet)
+    {
+        bullet.SetActive(false);
+        bulletPool.Enqueue(bullet);
+    }
+}
