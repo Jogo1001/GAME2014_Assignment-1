186edb0e289e4e408699a3c984b08a7d8d517a39
Jogo1001 <101247726@georgebrown.ca>
2025-10-09 13:50:38 -0400

    scrolling background bug fixed

--- GAME2014_Assignment1/Assets/Scripts/PlayerBehaviour.cs
+++ GAME2014_Assignment1/Assets/Scripts/PlayerBehaviour.cs
@@ -1,16 +1,74 @@
+using Unity.VisualScripting;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class PlayerBehaviour : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    InputAction MoveInput;
+    public Vector2 Direction;
+    public Vector2 Destination;
+    Camera camera;
+
+
+
+    [SerializeField]
+    InputActionAsset _playerController;
+
+    [SerializeField]
+    Boundary VerticalBoundary;
+
+    [SerializeField]
+    Boundary HorizontalBoundary;
+
+    [SerializeField]
+    public float speed;
+   
     void Start()
     {
-        
+        MoveInput = _playerController.FindAction("Move");
+        camera = Camera.main;
+    }
+    private void Update()
+    {
+
+        TouchScreenMove();
+       
+        CheckBoundaries();
     }
 
-    // Update is called once per frame
-    void Update()
+   
+    void TraditionalMove() 
     {
-        
+        Direction = MoveInput.ReadValue<Vector2>();
+        Debug.Log(Direction);
+        transform.position = new Vector3(transform.position.x + Direction.x * speed * Time.deltaTime
+                                         , transform.position.y + Direction.y * speed * Time.deltaTime
+                                         , transform.position.z);
+
+
+
     }
+
+    void TouchScreenMove() 
+    {
+  
+        Destination = camera.ScreenToWorldPoint(MoveInput.ReadValue<Vector2>());
+        transform.position = Vector3.Lerp(transform.position, Destination, speed * Time.deltaTime);
+       
+
+    }
+    public void CheckBoundaries()
+    {
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, HorizontalBoundary.min, HorizontalBoundary.max),
+                                          Mathf.Clamp(transform.position.y, VerticalBoundary.min, VerticalBoundary.max),
+                                          transform.position.z);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Debug.Log("You Got Hit!");
+        }
+
+    }
+
 }
